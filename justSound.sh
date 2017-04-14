ebd2d83f1056be49596d25f7fcb9635c3bf4dab6
Andrei Aldea <Grippentech@users.noreply.github.com>
2017-04-14 00:13:16 -0400

    fixed error

--- justSound.sh
+++ justSound.sh
@@ -1,7 +1,7 @@
 #Let's get sound working
 sudo apt-get update
 sudo apt-get install unzip -y
-wget -O -c  "https://github.com/Grippentech/Asus-E200HA-Linux-Post-Install-Script/releases/download/0.1/kernel.zip"
+wget -O "https://github.com/Grippentech/Asus-E200HA-Linux-Post-Install-Script/releases/download/0.1/kernel.zip"
 unzip kernel.zip
 cd kernel
 sudo dpkg -i *.deb
