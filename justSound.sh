a11e3a62a6130f15fae52ec5d5d71f51d87ac0ee
Andrei Aldea <Grippentech@users.noreply.github.com>
2017-04-13 23:05:45 -0400

    File to fix just sound as per rexuest

---
+++ justSound.sh
@@ -0,0 +1,12 @@
+#Let's get sound working
+sudo apt-get update
+sudo apt-get install unzip -y
+wget -O -c  "https://github.com/Grippentech/Asus-E200HA-Linux-Post-Install-Script/releases/download/0.1/kernel.zip"
+unzip kernel.zip
+cd kernel
+sudo dpkg -i *.deb
+tar -xvf chtcx2072x.tar
+cd chtcx2072x
+mkdir /usr/share/alsa/ucm/chtcx2072x/
+mv chtcx2072x.conf /usr/share/alsa/ucm/chtcx2072x/
+mv HiFi.conf /usr/share/alsa/ucm/chtcx2072x/
