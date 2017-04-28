2029f18ca4d40c9c3580b76d691e65a4e3b78d93
Vladimir Veysman <fireflashie@yandex.ru>
2017-04-28 13:10:19 +0300

    Auto-deletion of other kernel vesrions
    NEEDS TESTING

--- e200ha_with_sound.sh
+++ e200ha_with_sound.sh
@@ -1,6 +1,5 @@
 #Let's get sound working
-sudo apt-get update
-sudo apt-get install unzip -y
+sudo apt-get update && sudo apt-get install unzip -y
 wget "https://github.com/Grippentech/Asus-E200HA-Linux-Post-Install-Script/releases/download/0.1/kernel.zip"
 unzip kernel.zip
 cd kernel
@@ -15,9 +14,15 @@ sudo mv HiFi.conf /usr/share/alsa/ucm/chtcx2072x/
 sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target #Disable Suspend, broken on CherryTrail
 sudo sh -c 'echo -e "[Login]\nHandleLidSwitch=ignore\nHandleLidSwitchDocked=ignore" >> /etc/systemd/logind.conf' #Disable Sleep/Hybernate when Lid is Closed
 
-#Let's now install tlp and thermald to save you some battery life
-sudo apt-get install tlp thermald -y
 #Change fstab a little bit for better eMMC support, eMMC Conservation settings
 sed -i -e 's/errors=remount-ro 0/discard,noatime,errors=remount-ro 0/g' /etc/fstab
+
+#Let's now install tlp and thermald to save you some battery life
+sudo apt-get install tlp thermald -y
+
+#Let's remove other versions of the kernel, in case the version of the kernel installed on the system is newer
+dpkg -l linux-* | awk '/^ii/{ print $2}' | grep -v -e `echo "4.10.0-custom" | cut -f1,2 -d"-"` | grep -e [0-9] | xargs sudo apt-get -y purge
+sudo apt-get autoremove -y
+
 sleep 15 #wait 15 seconds
-sudo reboot now #Reboot to reenable Trackpad
+sudo reboot now
