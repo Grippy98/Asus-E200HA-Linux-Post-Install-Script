d74f9a2fc1a5c5d70637d0e17f8adf32e0ce9d9b
Andrei Aldea <Grippentech@users.noreply.github.com>
2017-06-07 10:22:54 -0400

    brought up to date with main Script

--- justSound.sh
+++ justSound.sh
@@ -1,7 +1,7 @@
 #Let's get sound working
 sudo apt-get update
 sudo apt-get install unzip -y
-wget "https://github.com/Grippentech/Asus-E200HA-Linux-Post-Install-Script/releases/download/0.1/kernel.zip"
+wget "https://github.com/Grippentech/Asus-E200HA-Linux-Post-Install-Script/releases/download/0.3/kernel.zip"
 unzip kernel.zip
 cd kernel
 sudo dpkg -i *.deb
@@ -10,3 +10,10 @@ cd chtcx2072x
 sudo mkdir /usr/share/alsa/ucm/chtcx2072x/
 sudo mv chtcx2072x.conf /usr/share/alsa/ucm/chtcx2072x/
 sudo mv HiFi.conf /usr/share/alsa/ucm/chtcx2072x/
+
+#Now cleanup the files
+sudo apt autoremove #Remove old versions of kernel
+cd ..
+cd .. 
+sudo rm -r kernel
+sudo rm kernel.zip
